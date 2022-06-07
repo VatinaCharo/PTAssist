00da4715d144a39c0a303a871365d382ad7a48b5
EnJoyXu <806056453@qq.com>
2022-06-08 00:26:43 +0800

    修改异常捕捉，修正了一个excel空行可能会引发的错误

--- Server/src/main/kotlin/nju/pt/kotlin/ext/ExcelExt.kt
+++ Server/src/main/kotlin/nju/pt/kotlin/ext/ExcelExt.kt
@@ -5,6 +5,7 @@ import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.slf4j.LoggerFactory
 import java.io.FileNotFoundException
+import kotlin.math.log
 
 fun Workbook.loadConfigFromExcel() = mutableListOf<Int>().apply {
     val logger = LoggerFactory.getLogger("Config Loader")
@@ -18,8 +19,9 @@ fun Workbook.loadConfigFromExcel() = mutableListOf<Int>().apply {
         // 读取每行信息
         configSheet.rowIterator().asSequence().forEach { row ->
             val cellValues = row.cellIterator().asSequence().map { it.toString() }.toList()
-            logger.info("cellValues = $cellValues")
-            try {
+            if (cellValues.size > 1) {
+                logger.info("cellValues = $cellValues")
+
                 when (cellValues[0]) {
                     //  Excel单元格默认采用浮点数记录，故读取出的字符串会包含"."，需要用substringBefore截去
                     "端口号" -> {
@@ -42,16 +44,21 @@ fun Workbook.loadConfigFromExcel() = mutableListOf<Int>().apply {
                         throw Exception("无法识别，请检查服务端配置信息！")
                     }
                 }
-            } catch (e: Exception) {
-                logger.error(e.message)
-                logger.error(e.stackTraceToString())
-//                    没看懂这个地方的异常抛出是什么意思，所有的异常都是这个原因？
-                throw Exception("裁判数和会场数必须是大于零的整数！")
             }
         }
     } catch (e: FileNotFoundException) {
         logger.error("未找到文件: ${e.message}")
         throw Exception("未找到文件: ${e.message}")
+    } catch (e: NumberFormatException) {
+        logger.error(e.message)
+        logger.error(e.stackTraceToString())
+        throw Exception("裁判数和会场数必须是大于零的整数！")
+    } catch (e: NullPointerException) {
+        logger.error("未找到sheet：" + e.message)
+        throw Exception("未找到sheet，请检查sheet名称")
+    } catch (e: Exception) {
+        logger.error(e.message)
+        throw Exception("服务端信息配置信息有误")
     }
 
     if (judgeCount <= 0) {
@@ -68,47 +75,59 @@ fun Workbook.loadConfigFromExcel() = mutableListOf<Int>().apply {
 fun Workbook.loadQuestionFromExcel() = mutableMapOf<Int, String>().apply {
     val logger = LoggerFactory.getLogger("Question Data Loader")
     logger.info("===================== loadQuestionFromExcel =====================")
+    try {
+        val questionSheet = this@loadQuestionFromExcel.getSheet(R.QUESTIONS_SHEET_NAME)
 
-    val questionSheet = this@loadQuestionFromExcel.getSheet(R.QUESTIONS_SHEET_NAME)
-
-    // 读取sheet内容
-    questionSheet.rowIterator().asSequence().forEachIndexed { rowIndex, row ->
-        //跳过第一行标题行
-        if (rowIndex != 0) {
-            val cellValues = row.cellIterator().asSequence().map { it.toString() }.toList()
-            logger.info("cellValues = $cellValues")
-            try {
-                this += (cellValues[0].substringBefore(".").toInt() to cellValues[1])
-                logger.info("Question Map = $this")
-            } catch (e: Exception) {
-                logger.error(e.message)
-                logger.error(e.stackTraceToString())
-                throw Exception("赛题信息填写有误！")
+        // 读取sheet内容
+        questionSheet.rowIterator().asSequence().forEachIndexed { rowIndex, row ->
+            //跳过第一行标题行
+            if (rowIndex != 0) {
+                val cellValues = row.cellIterator().asSequence().map { it.toString() }.toList()
+                if (cellValues.size > 1) {
+                    logger.info("cellValues = $cellValues")
+                    this += (cellValues[0].substringBefore(".").toInt() to cellValues[1])
+                    logger.info("Question Map = $this")
+                }
             }
         }
+    } catch (e: NullPointerException) {
+        logger.error("未找到sheet：" + e.message)
+        throw Exception("未找到sheet，请检查sheet名称")
+    } catch (e: Exception) {
+        logger.error(e.message)
+        logger.error(e.stackTraceToString())
+        throw Exception("赛题信息填写有误！")
     }
+
+
 }.toMap()
 
 fun Workbook.loadSchoolFromExcel() = mutableMapOf<Int, String>().apply {
     val logger = LoggerFactory.getLogger("School Data Loader")
     logger.info("===================== loadSchoolFromExcel =====================")
 
-    val schoolSheet = this@loadSchoolFromExcel.getSheet(R.SCHOOL_SHEET_NAME)
+    try {
+        val schoolSheet = this@loadSchoolFromExcel.getSheet(R.SCHOOL_SHEET_NAME)
 
-    // 读取sheet内容
-    schoolSheet.rowIterator().asSequence().forEachIndexed { rowIndex, row ->
-        //跳过第一行标题行
-        if (rowIndex != 0) {
-            val cellValues = row.cellIterator().asSequence().map { it.toString() }.toList()
-            logger.info("cellValues = $cellValues")
-            try {
-                this += (cellValues[0].substringBefore(".").toInt() to cellValues[1])
-                logger.info("School Map = $this")
-            } catch (e: Exception) {
-                logger.error(e.message)
-                logger.error(e.stackTraceToString())
-                throw Exception("学校信息填写有误！")
+        // 读取sheet内容
+        schoolSheet.rowIterator().asSequence().forEachIndexed { rowIndex, row ->
+            //跳过第一行标题行
+            if (rowIndex != 0) {
+                val cellValues = row.cellIterator().asSequence().map { it.toString() }.toList()
+                if (cellValues.size > 1) {
+                    logger.info("cellValues = $cellValues")
+                    this += (cellValues[0].substringBefore(".").toInt() to cellValues[1])
+                    logger.info("School Map = $this")
+                }
             }
         }
+    } catch (e: NullPointerException) {
+        logger.error("未找到sheet：" + e.message)
+        throw Exception("未找到sheet，请检查sheet名称")
+    } catch (e: Exception) {
+        logger.error(e.message)
+        logger.error(e.stackTraceToString())
+        throw Exception("学校信息填写有误！")
     }
+
 }.toMap()
