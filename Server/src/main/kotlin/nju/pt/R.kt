959b1a444123861f94167e0853a4b9c4c3613cb8
VatinaCharo <njuscimenphy@gmail.com>
2022-06-08 12:01:18 +0800

    modify data structure

--- Server/src/main/kotlin/nju/pt/R.kt
+++ Server/src/main/kotlin/nju/pt/R.kt
@@ -3,11 +3,12 @@ package nju.pt
 import java.io.File
 
 object R {
-    const val DEFAULT_PORT = 7890
+    // 默认的正反评三方的分数权重
+    val DEFAULT_WEIGHT_ARRAY = doubleArrayOf(3.0, 2.0, 1.0)
 
     private const val CONFIG_EXCEL_NAME = "server_config.xlsx"
     val CONFIG_EXCEL_FILE = File("./$CONFIG_EXCEL_NAME")
-    const val CONFIG_SHEET_NAME = "服务端配置"
+    const val CONFIG_SHEET_NAME = "软件配置"
     const val QUESTIONS_SHEET_NAME = "赛题信息"
     const val TEAM_SHEET_NAME = "队伍信息"
     const val SCHOOL_SHEET_NAME = "学校信息"
--- Server/src/main/kotlin/nju/pt/server/Data.kt
+++ Server/src/main/kotlin/nju/pt/server/Data.kt
@@ -8,33 +8,61 @@ import org.slf4j.LoggerFactory
 data class PlayerData(
     var id: Int,
     var name: String,
-    var gender: String,
-    var teamId: Int
+    var gender: String
 )
 
 
 data class TeamData(
     var id: Int,
     var name: String,
-    var schoolId: Int
+    var schoolId: Int,
+    val playerDataList: MutableList<PlayerData>,
+    val recordDataList: MutableList<RecordData>
 )
 
 /**
  * 一条比赛记录
  */
 data class RecordData(
-    var teamId: Int,
-    var playerId: Int,
     var roomId: Int,
     var round: Int,
     var phase: Int,
     var questionId: Int,
+    var playerId: Int,
     var role: Int,
     var score: Double,
-    var rFactor: Double,
-    var oFactor: Double,
-    var vFactor: Double
-)
+    var weightArray: DoubleArray = R.DEFAULT_WEIGHT_ARRAY
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as RecordData
+
+        if (roomId != other.roomId) return false
+        if (round != other.round) return false
+        if (phase != other.phase) return false
+        if (questionId != other.questionId) return false
+        if (playerId != other.playerId) return false
+        if (role != other.role) return false
+        if (score != other.score) return false
+        if (!weightArray.contentEquals(other.weightArray)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = roomId
+        result = 31 * result + round
+        result = 31 * result + phase
+        result = 31 * result + questionId
+        result = 31 * result + playerId
+        result = 31 * result + role
+        result = 31 * result + score.hashCode()
+        result = 31 * result + weightArray.contentHashCode()
+        return result
+    }
+}
 
 /**
  * 配置信息
