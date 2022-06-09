dce04375cc16e293bc8bf12584c0d6657aa8ab8e
EnJoyXu <806056453@qq.com>
2022-06-09 10:34:47 +0800

    读取Json文件

--- DatabaseAssist/src/main/kotlin/nju/pt/databaseassist/JsonInterface.kt
+++ DatabaseAssist/src/main/kotlin/nju/pt/databaseassist/JsonInterface.kt
@@ -1,9 +1,11 @@
 package nju.pt.databaseassist
 
+import kotlinx.serialization.decodeFromString
 import org.slf4j.LoggerFactory
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
+import java.io.FileNotFoundException
 
 object JsonInterface {
     fun toJson(teamDataList: TeamDataList,savePath :String){
@@ -13,5 +15,18 @@ object JsonInterface {
         File(savePath).writeText(format.encodeToString(teamDataList))
         logger.info("===================== JsonFileSavedSuccessfully =====================")
 
+    }
+
+    fun fromJson(readPath:String):TeamDataList{
+        val logger = LoggerFactory.getLogger("Read json file from: $readPath")
+        logger.info("===================== ReadingJsonFile =====================")
+        try {
+            val teamDataList =  Json.decodeFromString<TeamDataList>(File(readPath).readText())
+            logger.info("===================== JsonFileReadSuccessfully =====================")
+            return  teamDataList
+        }catch (e: FileNotFoundException) {
+            logger.error("未找到文件: ${e.message}")
+            throw Exception("未找到文件: ${e.message}")
+        }
     }
 }
