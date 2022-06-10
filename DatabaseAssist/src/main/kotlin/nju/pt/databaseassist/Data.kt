984ec864b7e9969257cea02733d81fe817f0b893
EnJoyXu <806056453@qq.com>
2022-06-10 18:46:03 +0800

    修改了学校信息的来源，现在从裁判信息中读取。 修改了学生id的来源，从1开始每次读取都增加1 增加了题号、题目名称检测是否重复以及队伍中的学校是否在裁判的学校列表中

--- DatabaseAssist/src/main/kotlin/nju/pt/databaseassist/Data.kt
+++ DatabaseAssist/src/main/kotlin/nju/pt/databaseassist/Data.kt
@@ -62,39 +62,91 @@ data class RecordData(
         return result
     }
 }
+
 @kotlinx.serialization.Serializable
-data class TeamDataList(var teamDataList:List<TeamData>, val questionMap:Map<Int,String>,val schoolMap:Map<Int,String>){
+data class TeamDataList(
+    var teamDataList: List<TeamData>,
+    val questionMap: Map<Int, String>,
+    val schoolMap: Map<Int, String>
+) {
     fun getTeamScore() = teamDataList.map {
         // 学校名，队伍名，总成绩
         Triple(
             schoolMap[it.schoolId],
             it.name,
             it.recordDataList.let { listData ->
-            var totalScore : Double = 0.0
-            listData?.forEach {recordData->
-                totalScore += recordData.score
-            }
-            totalScore
-        })
+                var totalScore: Double = 0.0
+                listData?.forEach { recordData ->
+                    totalScore += recordData.score
+                }
+                totalScore
+            })
     }.sortedByDescending { it.third }
 
 
-    fun getReviewTable() = teamDataList.map{
-        // 学校名，队伍名，[(赛题id，角色们),()]
+    fun getReviewTable() = teamDataList.map {
+        // 学校名，队伍名，[(赛题id to 角色们),()]
         Triple(
             schoolMap[it.schoolId],
             it.name,
-//            mutableListOf<Pair<Int,String>>().apply {
-//                    it.recordDataList?.forEach {recordData ->
-//                       this.add( recordData.questionId  to recordData.role)
-//                    }
-//            }.toList()
-            mutableMapOf<Int,String>().apply {
-                it.recordDataList?.forEach {recordData ->
-                    this[recordData.questionId] = this.getOrDefault(recordData.questionId,"")+ recordData.role
+            mutableMapOf<Int, String>().apply {
+                it.recordDataList?.forEach { recordData ->
+                    this[recordData.questionId] = this.getOrDefault(recordData.questionId, "") + recordData.role
                 }
             }
         )
     }
+
+    fun getPlayerScore() = teamDataList.map { it ->
+        // 学校名，队伍名，[(姓名 to [性别，正方得分总览，反方得分总览，评方得分总览,正方平均分,反方平均分,评方平均分])]
+        Triple(
+            schoolMap[it.schoolId],
+            it.name,
+            mutableMapOf<String, MutableList<Any>>().apply {
+                it.recordDataList?.forEach { recordData ->
+                    //若不是拒题
+                    if (recordData.role != "X") {
+                        //获取这条记录中的队员名称和性别
+                        val (playerName, playerGender) = it.playerDataList.filter { playerData ->
+                            playerData.id == recordData.playerId
+                        }[0].let { playerData ->
+                            listOf(playerData.name, playerData.gender)
+                        }
+                        //增加这条记录
+                        this[playerName] = this.getOrDefault(
+                            playerName, mutableListOf<Any>(
+                                playerGender,
+                                "", "", "",
+                                0.0,0.0,0.0
+                            )
+                        ).apply {
+                            when (recordData.role) {
+                                "R" -> {this[1] = this[1] as String + "," + recordData.score.toString()
+                                    this[1+3] = this[1+3] as Double + recordData.score}
+                                "O" -> {this[2] = this[2] as String + "," + recordData.score.toString()
+                                    this[2+3] = this[2+3] as Double + recordData.score}
+                                "V" -> {this[3] = this[3] as String + "," + recordData.score.toString()
+                                    this[3+3] = this[3+3] as Double + recordData.score}
+                            }
+                        }
+                    }
+
+                }
+
+                this.forEach { (name, dataList) ->
+                    for (i in 1..2){
+                        if (dataList[i] != ""){
+                            //求平均值
+                            dataList[i+3] = (dataList[i+3]as Double)/( dataList[i] as String).count {it ==',' }
+                            //去掉第一个,
+                            dataList[i] = (dataList[i] as String).substringAfter(",")
+                        }
+                    }
+                }
+
+            }.toMap()
+        )
+    }
+
 }
 
--- Server/src/main/kotlin/nju/pt/kotlin/ext/ExcelExt.kt
+++ Server/src/main/kotlin/nju/pt/kotlin/ext/ExcelExt.kt
@@ -1,5 +1,6 @@
 package nju.pt.kotlin.ext
 
+
 import nju.pt.R
 import nju.pt.databaseassist.JsonInterface
 import nju.pt.databaseassist.PlayerData
@@ -10,7 +11,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.slf4j.LoggerFactory
 import java.io.FileNotFoundException
 
-//TODO: 检查输入中有关序号的是否有重复，以及排序，是否以1为开始
+
 
 fun Workbook.loadConfigFromExcel() = mutableListOf<Any>().apply {
     val logger = LoggerFactory.getLogger("Config Loader")
@@ -98,9 +99,9 @@ fun Workbook.loadConfigFromExcel() = mutableListOf<Any>().apply {
 fun Workbook.loadQuestionFromExcel() = mutableMapOf<Int, String>().apply {
     val logger = LoggerFactory.getLogger("Question Data Loader")
     logger.info("===================== loadQuestionFromExcel =====================")
+    var qCount = 0
     try {
         val questionSheet = this@loadQuestionFromExcel.getSheet(R.QUESTIONS_SHEET_NAME)
-
         // 读取sheet内容
         questionSheet.rowIterator().asSequence().forEachIndexed { rowIndex, row ->
             //跳过第一行标题行
@@ -110,17 +111,30 @@ fun Workbook.loadQuestionFromExcel() = mutableMapOf<Int, String>().apply {
                     logger.info("cellValues = $cellValues")
                     this += (cellValues[0].substringBefore(".").toInt() to cellValues[1])
                     logger.info("Question Map = $this")
+                    qCount += 1
                 }
             }
         }
+        //判断题号是否有重复
+        if (qCount != this.size ){
+            logger.error("题号有重复")
+            throw Exception("题号有重复，请检查题号！")
+        }
+        //判断题目号是否有重复
+        if (qCount != this.values.distinct().size){
+            logger.error("题目名称有重复")
+            throw Exception("题号有重复，请检查题号！")
+        }
     } catch (e: NullPointerException) {
         logger.error("未找到sheet：" + e.message)
-        throw Exception("未找到sheet，请检查sheet名称")
+        throw Exception("未找到sheet，请检查sheet名称！")
     } catch (e: Exception) {
         logger.error(e.message)
         logger.error(e.stackTraceToString())
         throw Exception("赛题信息填写有误！")
     }
+
+
 
 
 }.toMap()
@@ -130,17 +144,19 @@ fun Workbook.loadSchoolFromExcel() = mutableMapOf<Int, String>().apply {
     logger.info("===================== loadSchoolFromExcel =====================")
 
     try {
-        val schoolSheet = this@loadSchoolFromExcel.getSheet(R.SCHOOL_SHEET_NAME)
+        val schoolSheet = this@loadSchoolFromExcel.getSheet(R.JUDGE_SHEET_NAME)
 
         // 读取sheet内容
+        var schoolIndex = 1
         schoolSheet.rowIterator().asSequence().forEachIndexed { rowIndex, row ->
             //跳过第一行标题行
             if (rowIndex != 0) {
                 val cellValues = row.cellIterator().asSequence().map { it.toString() }.toList()
                 if (cellValues.size > 1) {
                     logger.info("cellValues = $cellValues")
-                    this += (cellValues[0].substringBefore(".").toInt() to cellValues[1])
+                    this += (schoolIndex to cellValues[0])
                     logger.info("School Map = $this")
+                    schoolIndex += 1
                 }
             }
         }
@@ -156,11 +172,15 @@ fun Workbook.loadSchoolFromExcel() = mutableMapOf<Int, String>().apply {
 }.toMap()
 
 fun Workbook.loadJudgeFromExcel() = mutableMapOf<String, List<String>>().apply {
+
+    val schoolMap = this@loadJudgeFromExcel.loadSchoolFromExcel()
+
     val logger = LoggerFactory.getLogger("Judge Data Loader")
     logger.info("===================== loadJudgeFromExcel =====================")
 
     try {
         val judgeSheet = this@loadJudgeFromExcel.getSheet(R.JUDGE_SHEET_NAME)
+
 
         // 读取sheet内容
         judgeSheet.rowIterator().asSequence().forEachIndexed { rowIndex, row ->
@@ -168,6 +188,12 @@ fun Workbook.loadJudgeFromExcel() = mutableMapOf<String, List<String>>().apply {
             if (rowIndex != 0) {
                 val cellValues = row.cellIterator().asSequence().map { it.toString() }.toList()
                 if (cellValues.size > 1) {
+                    //检测裁判学校是否在提供的学校列表内
+                    if (!schoolMap.values.contains(cellValues[0])){
+                        logger.error("${cellValues[0]}并未在提供的学校信息内！")
+                        throw Exception("${cellValues[0]}并未在提供的学校信息内！")
+                    }
+
                     logger.info("cellValues = $cellValues")
                     this += (cellValues[0] to cellValues.subList(1, cellValues.size))
                     logger.info("School Map = $this")
@@ -194,6 +220,8 @@ fun Workbook.loadTeamFromExcel() = mutableListOf<TeamData>().apply {
         val teamSheet = this@loadTeamFromExcel.getSheet(R.TEAM_SHEET_NAME)
         val reversedSchoolMap = this@loadTeamFromExcel.loadSchoolFromExcel().entries.associate { (k, v) -> v to k }
         logger.info("===================== loadTeamFromExcel =====================")
+
+        var playerId = 1
         // 读取sheet内容
         teamSheet.rowIterator().asSequence().forEachIndexed { rowIndex, row ->
             //跳过第一行标题行
@@ -207,6 +235,11 @@ fun Workbook.loadTeamFromExcel() = mutableListOf<TeamData>().apply {
                         throw Exception("队员信息不全！")
                     }
 
+                    //检测队伍学校是否在提供的学校列表内
+                    if (!reversedSchoolMap.containsKey(cellValues[0])){
+                        logger.error("${cellValues[0]}并未在提供的学校信息内！")
+                        throw Exception("${cellValues[0]}并未在提供的学校信息内！")
+                    }
 
                     this += TeamData(
                         id = cellValues[2].substringBefore(".").toInt(),
@@ -218,12 +251,12 @@ fun Workbook.loadTeamFromExcel() = mutableListOf<TeamData>().apply {
                                 this.add(
                                     PlayerData(
                                         // 队员id的命名规则为 学校id*1000 + 队伍抽签号 *10 + 队内序号
-                                        id = reversedSchoolMap[cellValues[0]]!! * 1000 + cellValues[2].substringBefore(".")
-                                            .toInt() + index,
+                                        id = playerId,
                                         name = subCellValues[index],
                                         gender = subCellValues[index + 1]
                                     )
                                 )
+                                playerId += 1
                             }
                         },
                         recordDataList = null
@@ -232,6 +265,12 @@ fun Workbook.loadTeamFromExcel() = mutableListOf<TeamData>().apply {
                 }
             }
         }
+    //检测抽签号是否有重复
+    if (this.size != this.map { it.id }.distinct().size){
+        logger.error("抽签号有重复")
+        throw Exception("抽签号有重复，请检查队伍抽签号！")
+    }
+
     } catch (e: NullPointerException) {
         logger.error("未找到sheet：" + e.message)
         throw Exception("未找到sheet，请检查sheet名称")
@@ -244,8 +283,6 @@ fun Workbook.loadTeamFromExcel() = mutableListOf<TeamData>().apply {
         logger.error(e.stackTraceToString())
         throw Exception("队伍信息填写有误！")
     }
-
-
 }.toList()
 
 
