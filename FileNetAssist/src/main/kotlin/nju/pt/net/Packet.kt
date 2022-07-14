1a324d6a16245de02f96d2d50e21efac6696647e
Eur3ka <njuscimenphy@gmail.com>
2022-07-14 17:36:32 +0800

    文件传输数据包

--- FileNetAssist/src/main/kotlin/nju/pt/net/Packet.kt
+++ FileNetAssist/src/main/kotlin/nju/pt/net/Packet.kt
@@ -2,8 +2,19 @@ package nju.pt.net
 
 import nju.pt.databaseassist.Data
 
+/**
+ * Packet 数据包
+ *
+ * @property roomID 房间号
+ * @property phase 总阶段数
+ * @property teamIDMatchList 对阵表，由队伍id构成，第一位是第一阶段的正方，以此类推
+ * @property data 数据库
+ * @constructor Create empty Packet
+ */
 @kotlinx.serialization.Serializable
 data class Packet(
     val roomID: Int,
+    val phase: Int,
+    val teamIDMatchList: List<Int>,
     val data: Data
 )
